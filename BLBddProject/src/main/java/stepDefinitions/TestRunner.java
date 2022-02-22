06085e284ddc1e1edc7f2dbc7dceeaf09183596d
10694212 <10694212@LTILPUN21500279.lntinfotech.com>
2022-02-22 16:28:56 +0530

    fourth commit

--- BLBddProject/src/main/java/stepDefinitions/TestRunner.java
+++ BLBddProject/src/main/java/stepDefinitions/TestRunner.java
@@ -1,9 +1,12 @@
 package stepDefinitions;
 
 import org.junit.runner.RunWith;
-
+import org.testng.annotations.BeforeClass;
+import io.cucumber.testng.CucumberOptions;
 import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+//import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.TestNGCucumberRunner;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src\\test\\resources\\Features",
@@ -13,6 +16,11 @@ plugin= {"pretty","json:target/MyReports/report.json","junit:target/MyReports/re
 dryRun = false,
 publish=true)
 
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests{
+	public TestNGCucumberRunner testNGCucumberRunner;
+    @BeforeClass(alwaysRun = true)
+    public void setUpCucumber() {
+         testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+    }
 
 }
